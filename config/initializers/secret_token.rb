c14c63078711d0e7eeb697e061cdba64699923aa
Miłosz Osiński <mylosz@yahoo.pl>
2012-11-29 23:16:06 +0100

    pierwszy push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SklepRails3::Application.config.secret_token = 'de7a0c7b10481faf4f3f56bbb0a90a24091a8c39cb113456c0cb3443e56399081fad9c7a57181456d61b1460f2705d2a175a0acb1bc76857054d25ab9382f884'
