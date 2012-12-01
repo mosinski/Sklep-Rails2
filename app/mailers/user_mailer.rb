55a21611a86e4deeaa418e6ff7de1e327a06d3dd
Miłosz Osiński <mylosz@yahoo.pl>
2012-12-01 19:42:43 +0100

    poprawienie wysyłania maili

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,6 @@
 class UserMailer < ActionMailer::Base
-  def registration_confirmation(user)  
+  def registration_confirmation(user)
+    @user = user  
     mail(:to => user.email, :subject => "Witamy!", :from => "Sklep-Rails.pl")  
   end  
 end
