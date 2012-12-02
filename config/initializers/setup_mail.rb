429e394d52b93e45f0753573a5d1a9cddb802ae3
Miłosz Osiński <mylosz@yahoo.pl>
2012-12-02 17:57:06 +0100

    przekierowanie z mail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -6,4 +6,4 @@ ActionMailer::Base.smtp_settings = {
   :authentication       => "plain",  
   :enable_starttls_auto => true  
 }
-ActionMailer::Base.default_url_options[:host] = "http://sklep-rails.herokuapp.com"
+ActionMailer::Base.default_url_options[:host] = "sklep-rails.herokuapp.com"
