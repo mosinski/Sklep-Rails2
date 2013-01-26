bfc4bb0e3fcb0edf8a1d66e5df564d89dc5a7074
Miłosz Osiński <mylosz@yahoo.pl>
2013-01-26 01:17:18 +0100

    dodanie polskiej strefy czasowej

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,6 @@ module SklepRails3
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.time_zone = 'Warsaw'
   end
 end
